7707d2b712ca08bde30c6d4527f540e50b730455
Zagarce <cesar.zagar@gmail.com>
2023-12-08 15:58:29 -0600

    (+)

---
+++ javascript/script.js
@@ -0,0 +1,53 @@
+// comentarios de una sola línea
+
+/* comentarios multilínea */
+
+    // ALERT Y CONSOLE.LOG
+
+//alert("hola mundo");
+
+//console.log("Este es un mensaje para la consola");
+
+/* var nombre = prompt("Ingresa tu nombre");
+alert("Hola, " + nombre +". Bienvenido a la CH35"); */ 
+
+/* var numero1 = parseInt (prompt("Ingresa el primer número"));
+var numero2 = parseInt (prompt("Ingresa el segundo número"));
+
+console.log (" La suma de "+numero1+" + "+ numero2 +" es "+(numero1 + numero2) ); 
+console.log("La resta de "+numero1+" - "+numero2+" es "+(numero1 - numero2)); 
+console.log("La multiplicación de "+numero1+" x "+numero2+" es " +(numero1 * numero2)); 
+console.log("La división de "+numero1+" / "+numero2+" es " +(numero1 / numero2));
+
+    // *** CONDICONALES ***
+
+var edad = prompt("Ingresa tu edad:");
+
+if (edad >= 18) {
+    alert("Bienvenido a Somos Poker. ¡Disfruta de la página!");
+} else {
+    alert("Lo siento, debes ser mayor de 18 años para acceder a Somos Poker. Acceso denegado.");
+} */
+
+    // *** VARIABLES ***
+/* 
+scope es el alcance de nuestras variables
+    var, let, const : su diferencia está en el alcance
+    var : alcance global
+    let : alcance local
+    const : mantiene su valor
+
+string : cadena de texto (lleva comillas)
+number : número 
+boolena : true/false
+null : nulo
+undefined : dato no asignado o definido
+*/
+
+let nombre = "Daniel"; 
+let inditadosExtras = 2; 
+let esMayorEdad = true;
+let edad = 24; 
+let invitadoEspecial = null;
+let horaDeSalida = undefined;
+
